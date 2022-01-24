fd7af9ca88e1a60913d78956d1faa1e58b3fca01
Carlos Barbachano <carlos@carloss-imac.arw.local>
2022-01-24 16:11:08 -0500

    Todo App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const [items, setItems] = useState([]);
+
+  function handleSubmit() {
+    if (input === "") {
+      alert("Please enter a task");
+      return;
+    }
+    const item = {
+      id: Date.now(),
+      text: input,
+      complete: false,
+    };
+
+    setItems((oldList) => [...oldList, item]);
+
+    setInput("");
+  }
+
+  function handleToggle(id) {
+    let mapped = items.map((item) => {
+      if (item.id === Number(id)) {
+        return { ...item, complete: !item.complete };
+      } else {
+        return item;
+      }
+    });
+
+    setItems(mapped);
+  }
+
+  const handleDelete = (id) => {
+    const newList = items.filter((item) => item.id !== Number(id));
+
+    setItems(newList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>Todo List</h1>
+        <div className="d-flex mx-auto mb-3" style={{ maxWidth: "456px" }}>
+          <input
+            className="form-control me-3"
+            type="text"
+            placeholder="Text..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSubmit()}
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+        <ul className="todolist shadow">
+          {items.map((item) => {
+            return (
+              <>
+                <div className="d-flex justify-content-between p-3">
+                  <li
+                    className={item.complete ? "todo strike" : "todo"}
+                    onClick={() => handleToggle(item.id)}
+                    key={item.id}
+                    id={item.id}
+                  >
+                    {item.text}
+                  </li>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </>
+            );
+          })}
+        </ul>
+      </Container>
     </div>
   );
 }
